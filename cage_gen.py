cf6683bd489859790d2c62615fd5faae4e773cdb
gmacari <47388932+gmacari@users.noreply.github.com>
2020-01-08 11:21:15 +0100

    Update cage_gen.py
    clean up the code a bit

--- cage_gen.py
+++ cage_gen.py
@@ -8,6 +8,7 @@ from chempy import cpv
 from os import path
 pymol.finish_launching(['pymol', '-cq'])
 
+#function to calculate the center of mass of a PyMol obj. Modified from PyMol Wiki
 def centroid(selection='all', center=0, quiet=1):
 
     model = cmd.get_model(selection)
@@ -28,19 +29,18 @@ def centroid(selection='all', center=0, quiet=1):
 
     return centroid
 
-
 cmd.feedback("disable","all","actions")
 cmd.feedback("disable","all","results")
 cmd.feedback("disable","all","warnings actions")
 
 protName= path.basename(sys.argv[1])
 ligName = path.basename(sys.argv[2])
-#cmd.load('/home/gabriele/Documenti/rdkitDocking/all/aa2ar/receptor.pdb', protName)
-#cmd.load('/home/gabriele/Documenti/rdkitDocking/all/aa2ar/crystal_ligand.mol2', ligName)
 cmd.load(sys.argv[1], protName)
 cmd.load(sys.argv[2], ligName)
+
+#if needed to center the search space on the ligand use this line instead
 #(comX, comY, comZ) = centroid(ligName)
-(comX, comY, comZ) = centroid('receptor')
+(comX, comY, comZ) = centroid(protName)
 ((maxX, maxY, maxZ), (minX, minY, minZ)) = cmd.get_extent(protName)
-#print((maxX, maxY, maxZ), (minX, minY, minZ))
+
 print('center_x = %8.4f\ncenter_y = %8.4f\ncenter_z = %8.4f\nsize_x =  %8.4f\nsize_y =  %8.4f\nsize_z = %8.4f\n' % (comX, comY, comZ, abs(maxX-minX), abs(maxY-minY), abs(maxZ-minZ)))
